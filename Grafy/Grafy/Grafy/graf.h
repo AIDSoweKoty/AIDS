1992133e26b2e41b1ae5cc6954cc8ab2190e0be4
RealGerwant <34253611+RealGerwant@users.noreply.github.com>
2018-05-06 00:13:48 +0200

    ostatni start

---
+++ Grafy/Grafy/Grafy/graf.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "list.h"
+#include "LIFO.h"
+
+class graf
+{
+public:
+	int size = 1;
+	bool ** AdjacencyMatrix;
+	ListOfSucssesor *ListOfSucessor;
+	ListOfArches ListOfArches;
+	int ** Labes;
+	int * Sorted;
+	LIFO stosik;
+private:
+	void SetSize(int size);
+	void RandGraphInMatrix(	float density);
+	void FillListOfSucessorFormMatrix();
+	void FillListOfArchesFromMatrix();
+public:
+	graf(int s, float d);
+	~graf();
+	void PrintListOfArches();
+	void PrintAdjacenceyMatrix();
+	void PrintListOfSucessor();
+	void CheckAdjacencyMatrixDensity();
+	bool Sort(int * hel , int i);
+	void DFSsort();
+	void PrintLabelsArray();
+	void PrintTopologyOrder();
+	int CountReturnArchForListOfArches();
+	int CountReturnArchForAdjacencyMatrix();
+	int CountReturnArchForListOfsucessors();
+};
+
