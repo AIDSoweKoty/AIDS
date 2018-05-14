bf91327e9dfb3cfbd814cba04cdcf027c1680fad
0026 <tomaszch13@gmail.com>
2018-05-14 18:08:35 +0200

    dodanie wykrywania drogi Eulera

--- Algorytmy z powracaniem/Program/main.cpp
+++ Algorytmy z powracaniem/Program/main.cpp
@@ -34,6 +34,13 @@ class glowa{
 	}
 	void wypiszElementy(){
 		element *wk=this->w;
+		cout<<"**************";
+		if(this->ilosc==0){
+			cout<<"Brak cyklu Hamiltona!"<<endl;
+			cout<<"**************"<<endl;
+			return;
+		}
+		cout<<"Cykl Hamiltona:"<<endl;
 		while(wk->n){
 			//cout<<wk->n;
 			for(int i=0;i<this->r;i++){
@@ -46,6 +53,7 @@ class glowa{
 				cout<<wk->w[i];
 		}
 			cout<<endl;
+		cout<<"**************"<<endl;
 	}
 	
 };
@@ -56,6 +64,11 @@ class graf{
 	bool **w; 
 	glowa hamilton;
 	int *pom;
+	
+	int iloscKrawedzi;
+	int *obwEul;
+	int *stos;
+	bool **obwEulTmp;
 	
 	public:
 	graf(int r){
@@ -70,12 +83,46 @@ class graf{
 				this->w[i][j]=false;
 			}
 		}
-		//graf co na zajeciach 
+		//graf wype³nianie (ten co na zajeciach) 
+		/*
 		this->w[0][1]=this->w[0][2]=this->w[0][3]=true;
 		this->w[1][0]=this->w[1][2]=this->w[1][4]=true;
 		this->w[2][0]=this->w[2][1]=this->w[2][4]=true;
 		this->w[3][0]=this->w[3][4]=true;
 		this->w[4][1]=this->w[4][2]=this->w[4][3]=true;
+		*/
+		this->w[0][1]=this->w[0][2]=this->w[0][3]=this->w[0][4]=true;
+		this->w[1][0]=this->w[1][2]=true;				//= this->w[1][3] ¿eby sprawdzic czy istniene hamilton
+		this->w[2][0]=this->w[2][1]=this->w[2][3]=this->w[2][4]=true;
+		this->w[3][0]=this->w[3][2]=true;
+		this->w[4][0]=this->w[4][2]=true;
+		//koniec wypelniania grafu
+		
+		this->obwEulTmp = new bool*[r];
+		for(int i=0;i<r;i++){
+			this->obwEulTmp[i] = new bool[r];
+			for(int j=0;j<r;j++){
+				this->obwEulTmp[i][j] = this->w[i][j];
+				if(i==j){
+				this->obwEulTmp[i][j] = 0;	
+				} 
+			}
+		}
+		int tmp=0;
+		for(int i=0;i<this->r;i++){
+			for(int j=0;j<this->r;j++){
+				if(this->w[i][j]){
+					tmp++;
+				}
+			}
+		}
+		tmp/=2;
+		this->iloscKrawedzi=tmp;
+		this->obwEul=new int [tmp+1];
+		this->stos=new int [tmp+1];
+		for(int i=0;i<tmp+1;i++){
+			this->obwEul[i]=this->stos[i]=-1;
+		}
 		}
 	void pisz(){
 		for(int i=0;i<this->r;i++){
@@ -116,7 +163,7 @@ class graf{
 					int f;
 					
 					for(int j=0;j<this->r;j++){
-						if(this->pom[j]==i){		//czy byliœmy
+						if(this->pom[j]==i){		//czy byliomy
 							pom=0;
 							f=i;
 							break;
@@ -140,6 +187,55 @@ class graf{
 			}
 		}
 	}
+	void obwodEulera(){
+		int warunekKonieczny;
+		for(int i=0;i<this->r;i++){
+			warunekKonieczny=0;
+			for(int j=0;j<this->r;j++){
+				if(this->w[i][j]){
+					warunekKonieczny++;
+				}
+			}
+			
+			if(warunekKonieczny%2==1){
+				cout<<"Brak obwodu Eulera!"<<endl;
+				return;
+			}
+		}
+		int a;	//gdzie jestesmy
+		int licznikStosu;
+		int kolejnoscZdejmowania=0;
+		licznikStosu=0;
+		a=this->stos[0]=0;	
+		pocz:
+			if(licznikStosu==-1)
+				return;
+			for(int i=0;i<this->r;i++){
+				if(this->obwEulTmp[a][i]){
+					this->obwEulTmp[a][i]=0;
+					this->obwEulTmp[i][a]=0;
+					a=i;
+					this->stos[++licznikStosu]=a;
+					goto pocz;
+				}
+			}
+			this->obwEul[kolejnoscZdejmowania++]=this->stos[licznikStosu];
+			if(licznikStosu!=-1)
+				a=this->stos[licznikStosu--];
+			goto pocz;
+		}
+	void wypiszObwodEulera(){
+		if(this->obwEul[iloscKrawedzi]==-1){
+			cout<<"****************Brak obwodu Eulera!"<<endl;
+			cout<<"****************"<<endl;
+			return;
+		}
+		cout<<"****************Obwod Eulera!"<<endl;
+		for(int i=0;i<this->iloscKrawedzi+1;i++){
+		cout<<this->obwEul[i]<<" ";
+		}
+		cout<<endl<<"****************"<<endl;
+	}
 };
 
 
@@ -148,9 +244,13 @@ int main(){
 	int a,b,c;
 	graf graf(5);
 	graf.pisz();
-	graf.cyklHamiltona();
+	//graf.cyklHamiltona();
 	
-	graf.hamilton.wypiszElementy();
+	//graf.hamilton.wypiszElementy();
+	
+	graf.obwodEulera();
+	graf.wypiszObwodEulera();
+	
 	
 	return 0;
 }
