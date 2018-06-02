54ec0056ebaaff5959d1cafc5c9999f3595dc078
RealGerwant <34253611+RealGerwant@users.noreply.github.com>
2018-06-02 14:10:06 +0200

    Heurestyki zrobione

---
+++ Plecak/Program/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "cmake-build-debug/plecak.h"
+
+int main() {
+    plecak P(10,0.5f);
+    P.wypisz_plecak();
+    std::cout<<"GH1 "<<P.GH1()<<std::endl;
+    std::cout<<"GH2 "<<P.GH2()<<std::endl;
+    std::cout<<"GH3 "<<P.GH3()<<std::endl;
+    std::cout<<"GH4 "<<P.GH4()<<std::endl;
+    return 0;
+}
